bd552b74c7336188563173541cf068e4a2662928
Tushar <116259865+TusharLEARNSawant@users.noreply.github.com>
2022-12-02 20:38:01 +0530

    Update AlertRepository.java

--- AlertRepository.java
+++ AlertRepository.java
@@ -1,6 +1,6 @@
-package com.github.chandanv89.loganalyser.repository;
+package com.github.TusharLEARNSawant.demoLogger.repository;
 
-import com.github.chandanv89.loganalyser.model.persistence.Alert;
+import com.github.TusharLEARNSawant.demoLogger.model.Alert;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
