8b48b3cfd951260afb6146c2208115741fa498c0
Tushar <116259865+TusharLEARNSawant@users.noreply.github.com>
2022-12-02 20:08:13 +0530

    Update LogAnalyser.java

--- LogAnalyser.java
+++ LogAnalyser.java
@@ -1,4 +1,5 @@
-
+package com.github.TusharLEARNSawant.demoLogger;
+import com.github.TusharLEARNSawant.demoLogger.service.LogAnalyserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
